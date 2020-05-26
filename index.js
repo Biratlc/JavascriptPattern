2db293e184cd0d6f3bc2caa9b99a3535789220d1
biratlamichhane <lcbirat@gmail.com>
2020-05-26 00:02:20 -0500

    prototype/class pattern implemented

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+class car {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
+
+const Ford = new car(4, "focus", "grey");
+console.log(Ford);
