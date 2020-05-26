ca9b14296fbe3d2ab8b55657fe78a074f45a3f9f
biratlamichhane <lcbirat@gmail.com>
2020-05-26 00:56:41 -0500

    constructor pattern implementation

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ class car {
   }
 }
 
+class Suv extends car {
+  constructor(doors, engine, color) {
+    super(doors, engine, color);
+    this.wheels = 10;
+  }
+}
+
 const Ford = new car(4, "focus", "grey");
+const minisuv = new Suv(4, "focus", "grey");
+
 console.log(Ford);
+console.log(minisuv);
