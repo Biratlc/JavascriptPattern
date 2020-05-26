9ab6f41e53bb8ea1591a2cae10d2cdd3a3ad2c98
biratlamichhane <lcbirat@gmail.com>
2020-05-26 01:13:13 -0500

    Singleton Pattern Implemented

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+let instance = null;
 class car {
   constructor(doors, engine, color) {
-    this.doors = doors;
-    this.engine = engine;
-    this.color = color;
+    if (!instance) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;
+    } else {
+      return instance;
+    }
   }
 }
 
-class Suv extends car {
-  constructor(doors, engine, color) {
-    super(doors, engine, color);
-    this.wheels = 10;
-  }
-}
+// class Suv extends car {
+//   constructor(doors, engine, color) {
+//     super(doors, engine, color);
+//     this.wheels = 10;
+//   }
+// }
 
 const Ford = new car(4, "focus", "grey");
-const minisuv = new Suv(4, "focus", "grey");
+const Henry = new car(1, "simple", "noduplicate Instance");
+// const minisuv = new Suv(4, "focus", "grey");
 
 console.log(Ford);
-console.log(minisuv);
+console.log(Henry);
