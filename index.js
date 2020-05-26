918a4caa8c0e0b2952304d8176233e1f4dd00bcd
biratlamichhane <lcbirat@gmail.com>
2020-05-26 03:47:25 -0500

    Abstract facory pattern implementation

--- index.js
+++ index.js
@@ -16,8 +16,37 @@ class carFactory {
     }
   }
 }
+class Suv {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
 
-const factory = new carFactory();
-const Honda = factory.createCar("Honda");
+class suvFactory {
+  createSuv(type) {
+    switch (type) {
+      case "Volswagon":
+        return new Car(4, "auto", "grey");
+      case "Ferrari":
+        return new Car(8, "Auto", "Metallic Grey");
+    }
+  }
+}
 
-console.log(Honda);
+const carfactory = new carFactory();
+const suvfactory = new suvFactory();
+
+const autoManufacturer = (type, model) => {
+  switch (type) {
+    case "car":
+      return carfactory.createCar(model);
+    case "suv":
+      return suvfactory.createSuv(model);
+  }
+};
+
+const carry = autoManufacturer("suv", "Ferrari");
+
+console.log(carry);
